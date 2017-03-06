3959c998b95c39dab7580bf0901e835446b666f8
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-03-06 08:28:22 -0800

    Added more tests to find OEIS sequence

--- test_puzzle.py
+++ test_puzzle.py
@@ -16,13 +16,37 @@ class TestPuzzle(unittest.TestCase):
             'b': ['a', 'c', 'bExit'],
             'c': ['a', 'b']
         }
+        self.three_nodes_three_exits = {
+            'a': ['b', 'c', 'aExit'],
+            'b': ['a', 'c', 'bExit'],
+            'c': ['a', 'b', 'cExit']
+        }
         self.four_nodes_three_exits = {
             'a': ['b', 'c', 'd', 'aExit'],
             'b': ['a', 'c', 'bExit'],
             'c': ['d', 'b', 'a'],
             'd': ['a', 'c', 'dExit']
         }
-        self.seven_nodes_six_exits = {
+        self.four_nodes_four_exits = {
+            'a': ['b', 'c', 'd', 'aExit'],
+            'b': ['a', 'c', 'bExit'],
+            'c': ['d', 'b', 'a', 'cExit'],
+            'd': ['a', 'c', 'dExit']
+        }
+        self.four_nodes_four_exits_max_connections = {
+            'a': ['b', 'c', 'd', 'aExit'],
+            'b': ['a', 'c', 'd', 'bExit'],
+            'c': ['d', 'b', 'a', 'cExit'],
+            'd': ['a', 'c', 'b', 'dExit']
+        }
+        self.five_nodes_five_exits_max_connections = {
+            'a': ['b', 'c', 'd', 'e', 'aExit'],
+            'b': ['a', 'c', 'd', 'e', 'bExit'],
+            'c': ['d', 'b', 'a', 'e', 'cExit'],
+            'd': ['a', 'c', 'b', 'e', 'dExit'],
+            'e': ['a', 'c', 'b', 'd', 'eExit']
+        }
+        self.the_witness_seven_nodes_six_exits = {
             'a': ['b', 'f', 'g' 'aExit'],
             'b': ['a', 'c', 'g' 'bExit'],
             'c': ['b', 'd', 'g' 'cExit'],
@@ -45,10 +69,26 @@ class TestPuzzle(unittest.TestCase):
         self.assertEqual(10, len(self.puzzle.solve(
             self.three_nodes_two_exits)))
 
+    def test_solve_three_nodes_three_exits(self):
+        self.assertEqual(15, len(self.puzzle.solve(
+            self.three_nodes_three_exits)))
+
     def test_solve_four_nodes_three_exits(self):
         self.assertEqual(32, len(self.puzzle.solve(
             self.four_nodes_three_exits)))
 
-    def test_solve_seven_nodes_six_exits(self):
+    def test_solve_four_nodes_four_exits(self):
+        self.assertEqual(42, len(self.puzzle.solve(
+            self.four_nodes_four_exits)))
+
+    def test_solve_four_nodes_four_exits_max_connections(self):
+        self.assertEqual(64, len(self.puzzle.solve(
+            self.four_nodes_four_exits_max_connections)))
+
+    def test_solve_five_nodes_five_exits_max_connections(self):
+        self.assertEqual(325, len(self.puzzle.solve(
+            self.five_nodes_five_exits_max_connections)))
+
+    def test_solve_the_witness_seven_nodes_six_exits(self):
         self.assertEqual(116, len(self.puzzle.solve(
-            self.seven_nodes_six_exits)))
+            self.the_witness_seven_nodes_six_exits)))
