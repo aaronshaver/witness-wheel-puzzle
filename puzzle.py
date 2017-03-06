597b2873990ae29fd945227ef935aec2f319f646
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-03-05 18:37:30 -0800

    New class: Node

--- puzzle.py
+++ puzzle.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
 
 
+class Node:
+    def __init__(self, has_exit, name, connected_to):
+        self.has_exit = has_exit
+        self.name = name
+        self.connected_to = []
+        for node in connected_to:
+            self.connected_to.append(node)
+
+    def get_has_exit(self):
+        return self.has_exit
+
+    def get_name(self):
+        return self.name
+
+    def get_connected_to(self):
+        return self.connected_to
+
+    def set_connected_to(self, new_items):
+        self.connected_to = new_items
+
+
+class Wheel:
+    pass
+
+
 class Puzzle:
 
-    def hello():
-        return "hello"
+    def main():
+
+        out = "Nodes: 7. Exits: 6. Unique paths: 702."
+        print(out)
+        return out
 
 if __name__ == "main":
     main()
--- test_puzzle.py
+++ test_puzzle.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
 
-from puzzle import Puzzle
+from puzzle import *
 import unittest
 
 
 class TestPuzzle(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual('hello', Puzzle.hello())
+
+    def setUp(self):
+        self.node = Node(True, 'a', ['b', 'i', 'h'])
+
+    def test_node_has_exit(self):
+        self.assertTrue(self.node.get_has_exit())
+
+    def test_node_get_name(self):
+        self.assertEqual('a', self.node.get_name())
+
+    def test_node_get_connected_to(self):
+        self.assertEqual(['b', 'i', 'h'], self.node.get_connected_to())
+
+    def test_node_set_connected_to(self):
+        self.node.set_connected_to(['f', 'e', 'd'])
+        self.assertEqual(['f', 'e', 'd'], self.node.get_connected_to())
+
+    def test_main(self):
+        self.assertEqual(
+            'Nodes: 7. Exits: 6. Unique paths: 702.',
+            Puzzle.main())
