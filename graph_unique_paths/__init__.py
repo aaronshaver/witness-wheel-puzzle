e72ac48ba263556cbc687573168d3adc68411500
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-03-24 15:34:30 -0700

    No need for relative path

--- graph_unique_paths/__init__.py
+++ graph_unique_paths/__init__.py
@@ -1,2 +1,2 @@
 """ Calculates max unique paths for directed graphs that have start and end nodes """
-from .graph import _get_nodes, _find_all_paths, solve
+from graph import _get_nodes, _find_all_paths, solve
